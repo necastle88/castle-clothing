1ac41165bf43c4f3ff7a92765bcb8616c3fbb5c1
Nick <49137044+Nicholas-Castle@users.noreply.github.com>
2020-04-27 21:16:21 -0700

    Feat: Creating homepage
    Creating a homepage component that displays 5 sections you can shop
    in, hats, shoes, coats, men's, and women's.

---
+++ src/hompage.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+    <div className='home'>
+      <div className='dir-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>Hats</h1>
+            <span className='subtitle'>Shop Hats</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className='home'>
+      <div className='dir-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>Coats</h1>
+            <span className='subtitle'>Shop Coats</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className='home'>
+      <div className='dir-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>Shoes</h1>
+            <span className='subtitle'>Shop Shoes</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className='home'>
+      <div className='dir-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>Womens</h1>
+            <span className='subtitle'>Shop</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className='home'>
+      <div className='dir-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>Mens</h1>
+            <span className='subtitle'>Shop</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default HomePage;
