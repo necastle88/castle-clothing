3cb2d397286d1d9fc0c33baf8e5e348022059040
Nick <49137044+Nicholas-Castle@users.noreply.github.com>
2020-04-27 21:32:53 -0700

    style: styles for homePage
    Created sass styling for the homepage. The home page now
    contains row of five boxes with titles and sub titles in a separate
    box.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomePage from './HomePage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    
+    <div>
+      <HomePage />
     </div>
   );
 }
