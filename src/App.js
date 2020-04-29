ef7f5d06b9c5f09de1d52ee1d197f951fd5e8701
Nick <49137044+Nicholas-Castle@users.noreply.github.com>
2020-04-28 20:21:30 -0700

    Chore: Broke hompage into components
    Broke homepage down into smaller components for easier
    reuse.
    
    style: added styling
    
    -Added font styles to the homepage
    -Added transitions effects to the menu-item container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePage from './HomePage';
+import HomePage from './pages/hompage/homepage';
 import './App.css';
 
 function App() {
--- src/pages/hompage/homepage.js
+++ src/pages/hompage/homepage.js
@@ -1,59 +1,11 @@
 import React from 'react';
+import Directory from '../../components/directory/directory';
 import './homepage.styles.scss';
 
 const HomePage = () => {
   return (
-    <div>
     <div className='home'>
-      <div className='dir-menu'>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Hats</h1>
-            <span className='subtitle'>Shop Hats</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className='home'>
-      <div className='dir-menu'>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Coats</h1>
-            <span className='subtitle'>Shop Coats</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className='home'>
-      <div className='dir-menu'>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Shoes</h1>
-            <span className='subtitle'>Shop Shoes</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className='home'>
-      <div className='dir-menu'>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Womens</h1>
-            <span className='subtitle'>Shop</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className='home'>
-      <div className='dir-menu'>
-        <div className='menu-item'>
-          <div className='content'>
-            <h1 className='title'>Mens</h1>
-            <span className='subtitle'>Shop</span>
-          </div>
-        </div>
-      </div>
-    </div>
+      <Directory />
     </div>
   )
 }
