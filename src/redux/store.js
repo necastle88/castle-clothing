3769cc9f0780f16e14e7e786b079125da17523d6
Nick <49137044+Nicholas-Castle@users.noreply.github.com>
2020-05-20 21:10:54 -0700

    Chore: changing thunks to sagas

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware} from 'redux';
 import { persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
 
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware()
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
@@ -14,6 +17,8 @@ if (process.env.NODE_ENV === 'development') {
 
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+//sagaMiddleware.run()
 
 export const persistor = persistStore(store);
 
